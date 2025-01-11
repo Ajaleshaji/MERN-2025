eb35a85b673aaac1d4c09dee82139bea4228f6f3
Ajaleshaji <ajaleshbabu@gmail.com>
2025-01-11 09:08:09 +0530

    work

---
+++ fuctions.js
@@ -0,0 +1,15 @@
+/*var a=[10,20,30]
+var b={"count":70,"abs":1}
+for(let [i,j] of Object.entries(b) ){
+    console.log(i,j)
+}*/
+
+var c=10
+var d=2
+console.log(~c)
+console.log(c&d);
+console.log(c|d);
+console.log(c>>2);
+console.log(d<<2);
+console.log(c^d);
+console.log(c>>>2);
