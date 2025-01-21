4d3c7f28707b4a3e71aa2ec521439cd444fcd783
Ajaleshaji <ajaleshbabu@gmail.com>
2025-01-21 08:42:16 +0530

    works

---
+++ React/React_project/src/App.jsx
@@ -0,0 +1,19 @@
+import ClassCompEG from './Components/ClassComponents/ClassCompEG'
+import About from './Components/FunctionalComponent/About'
+import Gallery from './Components/FunctionalComponent/Gallery'
+import Home from './Components/FunctionalComponent/Home';
+import Contact from './Components/FunctionalComponent/Contact'
+
+function App() {
+  return (
+    <section>
+    <Home/>
+    <About/>
+    <ClassCompEG/>
+    <Gallery/>
+    <Contact/>
+    </section>
+)
+}
+
+export default App;
